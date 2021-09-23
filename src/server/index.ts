f352bfd7d399cb01b5b0d44a872ef02d90e14d90
Mark Bevels <mark.bevels@derived.studio>
2021-09-23 11:31:00 +0100

    Changed server port to 9100

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { startServer } from './server'
 
-const port = 9000
+const port = 9100
 const server = startServer({ app: express(), port })
 
 server.listen(port, () => {
